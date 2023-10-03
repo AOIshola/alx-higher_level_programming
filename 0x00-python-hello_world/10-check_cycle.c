63b370e2126546a813280a3437e3e7785a822487
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2023-10-03 00:23:02 -0700

    implement check cycle function

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a list has a cycle in it
+ * @list: points to the first node in list
+ *
+ * Return: 0 if there is no cycle, otherwise, 1
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *head, *slowPtr, *fastPtr = NULL;
+
+	head = slowPtr = fastPtr = list;
+
+	while (slowPtr != NULL)
+	{
+		slowPtr = slowPtr->next;
+		fastPtr = fastPtr->next->next;
+
+		if (slowPtr = fastPtr)
+		{
+			return (1);
+		}
+	}
+
+	return (0);
+}
