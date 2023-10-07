816640a2693667df4e464209ad23b194304a4f4b
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2023-10-07 00:50:35 -0700

    fix errors

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     str_len = len(sentence)
-    first = sentence[0]
+    if str_len == 0:
+        first = None
+    else:
+        first = sentence[0]
 
     len_n_first = (str_len, first)
     return (len_n_first)
