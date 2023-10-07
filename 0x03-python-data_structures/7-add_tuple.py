ed588c7196a99ecba4db48f12c47ae5aeedc62be
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2023-10-07 01:32:58 -0700

    fix errors

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,11 +3,6 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     tuple_1 = tuple_a + (0, 0)
     tuple_2 = tuple_b + (0, 0)
-    c = tuple(zip(tuple_a, tuple_2))
-    add_list = []
-    for i in range(2):
-        add = 0
-        for tup in c[i]:
-            add += tup
-        add_list.append(add)
-    return (tuple(add_list))
+    add_list = ()
+    add_list = tuple_1[0] + tuple_2[0], tuple_1[1] + tuple_2[1]
+    return (add_list)
