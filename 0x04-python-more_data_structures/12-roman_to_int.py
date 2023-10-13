16bc126ad3195efd0bda80894caebf3873afbc7f
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2023-10-13 14:43:29 -0700

    convert roman numerals to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
-    symbols = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
-            'D': 500, 'M': 1000}
+    syms = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     if not isinstance(roman_string, str) or not roman_string:
         return (0)
     result = 0
     prev = 0
     for symbol in roman_string:
-        if symbol in symbols:
-            if prev < symbols[symbol]:
-                result += (symbols[symbol] - (2 * prev))
-                prev = symbols[symbol]
+        if symbol in syms:
+            if prev < syms[symbol]:
+                result += (syms[symbol] - (2 * prev))
+                prev = syms[symbol]
             else:
-                prev = symbols[symbol]
-                result += symbols[symbol]
+                prev = syms[symbol]
+                result += syms[symbol]
     return (result)
