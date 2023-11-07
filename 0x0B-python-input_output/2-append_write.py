adbbe5406d789341d572f4580b14ec64ca829342
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2023-11-07 15:11:19 -0800

    use python i/o

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -4,10 +4,4 @@
 def append_write(filename="", text=""):
     """Appends to a file"""
     with open(filename, encoding="utf-8", mode="a") as f:
-        f.write(text)
-    with open(filename, encoding="utf-8") as f:
-        num_chars = 0
-        for line in f:
-            for char in line:
-                num_chars += 1
-    return (num_chars)
+        return (f.write(text))
