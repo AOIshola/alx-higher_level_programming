672aa0b94f34e3b74d21a88c16a7a9f966cb9157
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2023-11-09 13:17:01 -0800

    implement pascal's triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,27 +1,24 @@
 #!/usr/bin/python3
-"""The Pascal Triangle Module"""
+"""The Pascal's Traingle Module"""
 
 
 def pascal_triangle(n):
-    """Represents a list of lists of integers
-    representing a Pascal's triangle"""
-    tri = []
+    """Creates a Pascal's Traingle"""
+    tri = [[1]]
+
     if n <= 0:
+        return ([[]])
+    elif n == 1:
         return (tri)
-    for i in range(n):
-        res = []
-        if i == 0:
-            res.append(1)
-            tri.append(res)
-        elif i > 0:
-            if i == 1:
-                res = [1]
+    else:
+        for i in range(1, n):
+            res = [1]
             last = tri[-1]
             for j in range(i):
-                if j == 0 or j == i - 1:
+                if j == i - 1:
                     res.append(1)
+                    tri.append(res)
                 else:
-                    add = last[j - 1] + last[j]
+                    add = last[j] + last[j + 1]
                     res.append(add)
-            tri.append(res)
-    return (tri)
+        return (tri)
