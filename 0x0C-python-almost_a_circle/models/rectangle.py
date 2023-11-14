05414f12b819e913ad6d84518ac4bd87cb662848
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2023-11-13 16:06:19 -0800

    make it almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -13,12 +13,12 @@ class Rectangle(Base):
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
         if width <= 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("width must be > 0")
         self.width = width
         if not isinstance(height, int):
             raise TypeError("height must be an integer")
         if height <= 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError("height must be > 0")
         self.height = height
         if not isinstance(x, int):
             raise TypeError("x must be an integer")
