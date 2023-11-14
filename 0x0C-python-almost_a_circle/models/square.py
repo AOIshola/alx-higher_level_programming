683f437d92fb4c534b10c1db0a4c0e9b4dd9e28f
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2023-11-13 16:09:16 -0800

    make it almost a circle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -32,7 +32,7 @@ class Square(Rectangle):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("width must be > 0")
         self.__size = value
 
     def update(self, *args, **kwargs):
