6cf1e63aced78095125a883f5fbb84d5fee28826
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2023-12-14 07:57:14 -0800

    write SQL queries

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,7 @@
 -- Lists all genres of the show Dexter
 SELECT tv_genres.name AS name
   FROM tv_shows
-	LEFT JOIN tv_show_genres
+	JOIN tv_show_genres
 	  ON tv_shows.id = tv_show_genres.show_id
 	LEFT JOIN tv_genres
 	  ON genre_id = tv_genres.id
