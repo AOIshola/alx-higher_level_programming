de19faf7bcb485d8f77116acf830332d335417c1
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2024-01-08 11:56:16 -0800

    warm up for JavaScript

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,18 +2,25 @@
 
 const args = process.argv;
 const argc = process.argv.length;
-const arr = [];
-let i = 2;
+//const arr = [];
+//let i = 2;
 
-if (argc === 1 || argc === 0) {
-  console.log(0);
+function secondMax(arr) {
+  const argc = arr.length;
+  let i = 2;
+  if (argc === 2 || argc === 3) {
+    return (0);
+  }
+
+  let newArr = [];
+  while (i < argc) {
+    const num = parseInt(arr[i]);
+    newArr.push(num);
+    i++;
+  }
+
+  const max2 = newArr.sort();
+  return (max2[max2.length - 2]);
 }
 
-while (i < argc) {
-  const num = parseInt(args[i]);
-  arr.push(num);
-  i++;
-}
-
-const max2 = arr.sort();
-console.log(max2[max2.length - 2]);
+console.log(secondMax(args));
