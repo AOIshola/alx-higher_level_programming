309e08e4a76c3765966c50ba6e8bcc383e7f2843
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2024-01-09 19:23:38 -0800

    use javascript objects

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -4,9 +4,8 @@ module.exports = class Rectangle {
   constructor (w, h) {
     if (!w || w <= 0 || !h || h <= 0) {
       return;
-    } else {
-      this.width = w;
-      this.height = h;
     }
+    this.width = w;
+    this.height = h;
   }
 };
