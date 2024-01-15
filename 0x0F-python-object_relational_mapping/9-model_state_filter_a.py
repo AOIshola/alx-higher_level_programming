723b973c933bfb9b0828b87b3273f320e7eb7966
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2024-01-14 21:51:31 -0800

    use python orm

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""List States module"""
+
+if __name__ == "__main__":
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.ext.declarative import declarative_base
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        sys.argv[1],
+        sys.argv[2],
+        sys.argv[3]
+        ))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).filter(State.name.like('%a%')).order_by(State.id)
+    for state in states:
+#    if not states:
+ #       print("Nothing")
+  #  else:
+        print("{}: {}".format(state.id, state.name))
