bb139cbfa149001b44c83579039de71e8362c5f1
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2024-01-25 15:51:22 -0800

    find a peak in array

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,9 +3,11 @@
 
 def find_peak(list_of_integers):
     """Finds the peak of an unsorted list of integers"""
-    low, high = 0, len(list_of_integers) - 1
+    low, high = 0, len(list_of_integers)
     n = len(list_of_integers) - 1
 
+    if len(list_of_integers) == 0:
+        return None
     while low <= high:
         mid = (low + high) // 2
         if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
