b327465c14981e62442a6a1efa548afb5eaa7326
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2024-01-25 13:03:13 -0800

    make get http request

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a GET request to the URL, and displays the body of the response
+# takes in a URL, sends a GET request to the URL, and displays the body of the http response
 response=$(curl -sI -o /dev/null -w "%{http_code}" $1); [ $response -eq 200 ] && curl -s $1
