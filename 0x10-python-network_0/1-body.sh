9151bf634471cbdfae8aa662b716938d22239097
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2024-01-25 13:02:04 -0800

    make get httpWq request

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a GET request to the URL, and displays the body of the http response
-[ $(curl -sI -o /dev/null -w "%{http_code}" $1) -eq 200 ] && curl -s $1
+response=$(curl -sI -o /dev/null -w "%{http_code}" $1); [ $response -eq 200 ] && curl -s $1
