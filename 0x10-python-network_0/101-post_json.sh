2c1b770f20c0b8c074fc0615775fe3134bcd1a41
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2024-01-25 23:06:25 -0800

    make post request with a JSON file

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
-curl -s -X POST -d @$2 -H "content-type: text/html" $1
+curl -s -X POST -d @"$2" -H "content-type: application/json" $1
