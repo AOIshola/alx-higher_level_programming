6bbf8e7c1a3ebea687fbf31f4be414e02721cca4
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2024-01-30 13:41:59 -0800

    send a request to a URL

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -8,4 +8,7 @@ import sys
 
 if __name__ == "__main__":
     req = requests.get(sys.argv[1])
-    print(req.headers["X-Request-Id"])
+    try:
+        print(req.headers["X-Request-Id"])
+    except KeyError as e:
+        pass
