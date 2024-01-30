3ccc6b6a40df78d5fe16f164af708bb93ef8511e
Ishola Abdulquadri <ishola.o.abdulquadri@gmail.com>
2024-01-30 08:33:48 -0800

    request response headers with urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
     with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as res:
         html = res.read()
     utf8 = html.decode("utf-8")
-    output = "Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
-    print(output.format(type(html), html, utf8))
+    out = "Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
+    print(out.format(type(html), html, utf8))
